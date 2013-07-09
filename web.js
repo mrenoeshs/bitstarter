f64fe92c3ae9b0002e5c3b2e4520bc1e91eba000
mrenoeshs <steven.e.eno@gmail.com>
2013-07-09 16:22:36 +0000

    Update web.js to fix index call

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync(index.html)));
+  response.send(buf.toString(fs.readFileSync('/home/ubuntu/hw3/bitstarter/index.html')));
 });
 
 var port = process.env.PORT || 5000;
